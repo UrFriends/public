46d521ff365a6f5668b7a72a6f761c20640c261e
jayhcrawford <141302428+jayhcrawford@users.noreply.github.com>
2025-08-31 17:29:22 -0700

    fixes config for staging

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -1,10 +1,9 @@
 import { FirebaseApp, getApp, getApps, initializeApp } from "@firebase/app";
 import { Auth, getAuth } from "@firebase/auth";
 import { Firestore, getFirestore } from "@firebase/firestore";
-import { DEV_API_KEY } from "../../secrets";
 
 const firebaseConfig = {
-  apiKey: DEV_API_KEY,
+  apiKey: process.env.NEXT_PUBLIC_STAGING_API_KEY,
   authDomain: "urfriends-beta.firebaseapp.com",
   projectId: "urfriends-beta",
   storageBucket: "urfriends-beta.firebasestorage.app",
